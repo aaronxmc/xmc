311d2bd39bd502b4cf81f39a6ed30edbede9db8a
xmc <xmc@CameldeMacBook-Pro.local>
2017-07-04 10:28:20 +0800

    加入显示天气信息的功能。

---
+++ CoolWeather/app/src/main/java/com/example/xmc/coolweather/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.example.xmc.coolweather.gson;
+
+/**
+ * Created by xmc on 2017/7/3.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
---
+++ CoolWeather/app/src/main/java/com/example/xmc/coolweather/gson/Suggestion.java
@@ -0,0 +1,39 @@
+package com.example.xmc.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by xmc on 2017/7/3.
+ */
+
+public class Suggestion {
+    @SerializedName("comf")
+    public Comfort comfort;
+
+    @SerializedName("cw")
+    public CarWash carWash;
+
+    public Sport sport;
+
+    public class Comfort {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+
+    public class CarWash {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+
+    public class Sport {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+
+}
